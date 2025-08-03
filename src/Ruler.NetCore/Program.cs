61ca2530b1e6b23896c5140b548cf85f02ea8939
Edi Wang <Edi.Wang@outlook.com>
2025-08-03 12:49:24 +0800

    Refactor namespace and update version to 2.2.0
    Changed the namespace from Ruler.NetCore to Ruler in MainForm.cs and Program.cs for consistency and modernization. Updated project version in Ruler.csproj from 2.1.0 to 2.2.0.

--- src/Ruler.NetCore/Program.cs
+++ src/Ruler.NetCore/Program.cs
@@ -1,23 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+namespace Ruler;
 
-namespace Ruler.NetCore
+static class Program
 {
-    static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
-        }
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
     }
 }
